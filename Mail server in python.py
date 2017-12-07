5c5eb5efc54c2343807760152e08ddabeabd7f4e
AcrobatAHA <ahanik94@gmail.com>
2017-12-07 19:51:19 +0600

    Add files via upload

---
+++ Mail server in python.py
@@ -0,0 +1,7 @@
+import smtplib
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.ehlo()
+server.starttls()
+server.login('email','password')
+server.sendmail('From mail','To mail','SMS')
+server.close()
